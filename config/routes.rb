e5094ca8edaa77c19a5c13eed633e11a5b9ec08a
BBwanaz <bbwanakocha@usapglobal.org>
2020-07-13 20:23:08 -0700

    Untracked

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-	
-  get 'subjects' , to: "pages#subjects"
-  get 'contact' , to: "pages#contact"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root to: 'pages#home'
-end
