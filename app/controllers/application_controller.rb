73589186d7d6f323575ebe51c4c02147b89ab5e3
BBwanaz <bbwanakocha@usapglobal.org>
2020-07-17 14:51:38 -0700

    Copyright to the website added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+	before_action :set_copyright
+
+def set_copyright
+	@copyright = FundoViewTool::Renderer.copyright 'Brandon Bwanakocha', 'All Rights Reserved'
+ end 
 end
+
+
+
+module FundoViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
+end 
