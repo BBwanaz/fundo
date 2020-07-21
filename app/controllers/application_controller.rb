2a50a1392fa3553e16ca943d7f7b28147c38d5bf
BBwanaz <bbwanakocha@usapglobal.org>
2020-07-21 08:07:58 -0700

    Added Name and Surname to registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
 	before_action :set_copyright
+
+def configure_permitted_parameters
+	devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :surname])
+end
 
 def set_copyright
 	@copyright = FundoViewTool::Renderer.copyright 'Brandon Bwanakocha', 'All Rights Reserved'
