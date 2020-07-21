476b858a71d70a51500b5163cc4d60180664f164
BBwanaz <bbwanakocha@usapglobal.org>
2020-07-20 20:14:15 -0700

    Users created, roles added to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	
-  devise_for :students
+  devise_for :users
   get 'subjects' , to: "pages#subjects"
   get 'contact' , to: "pages#contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/migrate/20200721023847_devise_create_users.rb
+++ db/migrate/20200721023847_devise_create_users.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class DeviseCreateStudents < ActiveRecord::Migration[6.0]
+class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :students do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -36,9 +36,9 @@ class DeviseCreateStudents < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :students, :email,                unique: true
-    add_index :students, :reset_password_token, unique: true
-    # add_index :students, :confirmation_token,   unique: true
-    # add_index :students, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
