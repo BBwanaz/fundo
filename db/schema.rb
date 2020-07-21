2fc46a9e077395a0d8b2d263b44b892de8969473
BBwanaz <bbwanakocha@usapglobal.org>
2020-07-20 20:37:23 -0700

    Removed students and tutors from schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,30 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_07_21_025835) do
 
-  create_table "students", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_students_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
-  end
-
-  create_table "tutors", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_tutors_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_tutors_on_reset_password_token", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
