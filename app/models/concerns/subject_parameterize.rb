f1cd77c2cc7eae356f520b411d726843f038bf19
BBwanaz <bbwanakocha@usapglobal.org>
2020-08-10 08:14:56 -0700

    friendly id updated

--- app/models/concerns/subject_parameterize.rb
+++ app/models/concerns/subject_parameterize.rb
@@ -3,7 +3,7 @@ module SubjectParameterize
 
  included do 
     def subject_id
-    "#{subject.title}".parameterize
+    "#{subject.title}-" + "#{session}-" + "#{year}".parameterize
   end
  end
 end
