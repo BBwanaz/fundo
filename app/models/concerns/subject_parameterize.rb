137a6da8cdaf5dd81e6f172d7c1eb2a91870c198
BBwanaz <bbwanakocha@usapglobal.org>
2020-08-10 08:24:10 -0700

    checks for image

--- app/models/concerns/subject_parameterize.rb
+++ app/models/concerns/subject_parameterize.rb
@@ -3,7 +3,7 @@ module SubjectParameterize
 
  included do 
     def subject_id
-    "#{subject.title}-" + "#{session}-" + "#{year}".parameterize
+    "#{subject.title}-" + "#{session}-" + "#{year}-" + "#{id}".parameterize
   end
  end
 end
--- app/models/query.rb
+++ app/models/query.rb
@@ -1,10 +1,13 @@
 class Query < ApplicationRecord
 	extend FriendlyId
+	validates_presence_of :image
+	validates_presence_of :profile
 
 	enum status:{active: 0, accepted: 1, completed: 2}
+	
 	belongs_to :subject
 	belongs_to :profile
-	validates_presence_of :profile
+	
 	mount_uploader :image, ImageUploader
     friendly_id :subject_id, use: :slugged
 
